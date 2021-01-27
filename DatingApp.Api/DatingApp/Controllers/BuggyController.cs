efe52833dddaf86856d77ce9d7c4e663b89c2f7f
Mehran123 <jaafari.mehran@web.de>
2021-01-27 20:57:24 +0100

    change 270120

---
+++ DatingApp.Api/DatingApp/Controllers/BuggyController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Data;
+using Api.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        private DataContext _context;
+
+        public BuggyController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<string> GetSecret()
+        {
+            return "secret";
+        } 
+        
+        [HttpGet("not-found")]
+        public ActionResult<string> GetNotFound()
+        {
+            var result = _context.Users.Find(-1);
+            if (result == null) return NotFound();
+
+            return Ok(result);
+        } 
+        
+        [HttpGet("Server-Error")]
+        public ActionResult<string> GetServerError()
+        {
+            var result = _context.Users.Find(-1);
+            var resultToReturn = result.ToString();
+
+            return resultToReturn;
+        } 
+        
+        [HttpGet("bad-request")]
+        public ActionResult<string> GetBadRequest()
+        {
+            return BadRequest("This was not a good request");
+        }
+    }
+}
---
+++ DatingApp.Api/DatingApp/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Api.Error;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private IHostEnvironment _env;
+        private ILogger<ExceptionMiddleware> _logger;
+        private RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+        {
+            _env = env;
+            _logger = logger;
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception exp)
+            {
+                _logger.LogError(exp, exp.Message);
+                context.Response.ContentType = @"applicaton/json";
+                context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+
+                var response = _env.IsDevelopment()
+                    ? new ApiException(context.Response.StatusCode, exp.Message, exp.StackTrace?.ToString())
+                    : new ApiException(context.Response.StatusCode, "Interal Server Error");
+
+                var options = new JsonSerializerOptions {PropertyNamingPolicy = JsonNamingPolicy.CamelCase};
+                var json = JsonSerializer.Serialize(response, options);
+                await context.Response.WriteAsync(json);
+            }
+        }
+    }
+}
